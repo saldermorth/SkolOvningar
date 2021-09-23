ac224dfb8060c4bcb54d0ceb49da88f60958ebf6
saldermorth <77542775+saldermorth@users.noreply.github.com>
2021-09-23 15:22:25 +0200

    11
    11

--- ÖvningsUppgifter OOP - A b/ÖvningsUppgifter OOP - A/Animal.cs
+++ ÖvningsUppgifter OOP - A b/ÖvningsUppgifter OOP - A/Animal.cs
@@ -14,6 +14,10 @@ namespace ÖvningsUppgifter_OOP___A
         public int Age { get; set; }
         public int NumbersOfLegs { get; set; }
         public bool Fast { get; set; }
+        public Animal()
+        {
+
+        }
         public Animal(string _type, string _name, int _yearBorn, int _age, int _numberOfLegs, bool _fast)
         {
             Type = _type;
@@ -23,17 +27,7 @@ namespace ÖvningsUppgifter_OOP___A
             NumbersOfLegs = _numberOfLegs;
             Fast = _fast;
         }
-        public string IsFast()
-        {
-            if (Fast)
-            {
-                return "Fast";
-            }
-            else
-            {
-                return "Slow";
-            }
-        }
+        
 
 
     }
--- ÖvningsUppgifter OOP - A b/ÖvningsUppgifter OOP - A/Program.cs
+++ ÖvningsUppgifter OOP - A b/ÖvningsUppgifter OOP - A/Program.cs
@@ -16,16 +16,80 @@ namespace ÖvningsUppgifter_OOP___A
       Det var en gång en <ålder> år gammal <djursort> som hette
       <namn>. En dag var <namn> ute på en promenade I skogen, och
       mötte en stor varg. Vargen bet av ett ben. <namn> sprang <snabbt
-      eller långsamt om snabb=true eller false> hem på sina <antalben –1>. Så var sagan slut.*/
+      eller långsamt om snabb=true eller false> hem på sina <antalben –1>. Så var sagan slut.
+    OOP : A-a
+     Skapa en klass, som innehåller följande properties
+     - Djursort (string)
+     - Namn (string)
+     - Födelseår (int)
+     - Ålder (int)
+     - Antal ben (int)
+     - Snabb (bool)
+     - (alla namn ska vara på engelska)
+     Utgå från exemplet med PersonStory och skapa en saga:
+     Det var en gång en <ålder> år gammal <djursort> som hette
+     <namn>. En dag var <namn> ute på en promenade I skogen, och
+     mötte en stor varg. Vargen bet av ett ben. <namn> sprang <snabbt
+     eller långsamt om snabb=true eller false> hem på sina <antalben –1>. Så var sagan slut. 
+    OOP: A-b
+     - Sagan ska byggas av en separat metod. 
+    - Sagan ska skrivas ut av en separat metod. 
+    - Ålder ska räknas ut av en separat metod
+     - Antal ben – 1 ska byggas av en separat metod
+     - Djuret ska få sina properties I en separat metod
+     - Main-metoden ska se ut så här enbart. static void Main(string[] args)
+     {
+     TellStory();
+     }*/
     class Program
     {
         static void Main(string[] args)
         {
-           Animal bobbY = new Animal("Dog","Bobby", 2015, 4,4, true);
 
-            Console.WriteLine($@"Det var en gång en {bobbY.Age} år gammal {bobbY.Type} som hette {bobbY.Name}. En dag var {bobbY.Name} ute på en promenade I skogen, och mötte en stor varg. Vargen bet av ett ben. {bobbY.Name} sprang {bobbY.IsFast()} hem på sina {bobbY.NumbersOfLegs - 1}.Så var sagan slut.");
+            TellStory();
+            
 
             Console.ReadKey();
+        }
+        public static void TellStory()
+        {
+            Animal bobbY = new Animal();
+            CreateObject(bobbY);
+            GetAge(bobbY);
+            string S = StringMaker(bobbY);
+            PrintStory(S);
+
+        }
+        public static Animal CreateObject(Animal a)
+        {
+            a.YearBorn = 2017;
+            a.Age = GetAge(a);             
+            a.Name = "Bobby";
+            a.Type = "Dog";
+            a.NumbersOfLegs = 4;
+            a.NumbersOfLegs = NumLegs(a);
+            a.Fast = true;
+            return a;
+
+        }
+        public static string StringMaker(Animal a)
+        {
+            string S = $@"Det var en gång en {a.Age} år gammal {a.Type} som hette {a.Name}. En dag var {a.Name} ute på en promenade I skogen, och mötte en stor varg. Vargen bet av ett ben. {a.Name} sprang {a.Fast} hem på sina {a.NumbersOfLegs} ben.Så var sagan slut.";
+            return S;
+        }
+        public static int GetAge(Animal a)
+        {
+            int age = DateTime.Now.Year - a.YearBorn;
+            return age;
+        }
+        public static void PrintStory(string s)
+        {
+            Console.WriteLine(s);
+        }
+        public static int NumLegs(Animal a)
+        {
+            int num = a.NumbersOfLegs - 1;
+            return num;
         }
     }
 }
