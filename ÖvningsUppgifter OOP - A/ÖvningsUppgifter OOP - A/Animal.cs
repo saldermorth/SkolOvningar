0cdf738c294ec0fa7467f417b2f21da47a67fb21
saldermorth <77542775+saldermorth@users.noreply.github.com>
2021-09-23 12:35:30 +0200

    Uppgift A
    A klar

---
+++ ÖvningsUppgifter OOP - A/ÖvningsUppgifter OOP - A/Animal.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ÖvningsUppgifter_OOP___A
+{
+    public class Animal
+    {   
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public int YearBorn { get; set; }
+        public int Age { get; set; }
+        public int NumbersOfLegs { get; set; }
+        public bool Fast { get; set; }
+        public Animal(string _type, string _name, int _yearBorn, int _age, int _numberOfLegs, bool _fast)
+        {
+            Type = _type;
+            Name = _name;
+            YearBorn = _yearBorn;
+            Age = _age;
+            NumbersOfLegs = _numberOfLegs;
+            Fast = _fast;
+        }
+        public string IsFast()
+        {
+            if (Fast)
+            {
+                return "Fast";
+            }
+            else
+            {
+                return "Slow";
+            }
+        }
+
+
+    }
+}
---
+++ ÖvningsUppgifter OOP - A/ÖvningsUppgifter OOP - A/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ÖvningsUppgifter_OOP___A
+{
+
+    /*Skapa en klass, som innehåller följande properties
+    - Djursort (string)
+    - Namn (string)
+    - Födelseår (int)
+    - Ålder (int)
+    - Antal ben (int)
+    - Snabb (bool)
+    - (alla namn ska vara på engelska)
+    
+       Utgå från exemplet med PersonStory och skapa en saga:
+      Det var en gång en <ålder> år gammal <djursort> som hette
+      <namn>. En dag var <namn> ute på en promenade I skogen, och
+      mötte en stor varg. Vargen bet av ett ben. <namn> sprang <snabbt
+      eller långsamt om snabb=true eller false> hem på sina <antalben –1>. Så var sagan slut.*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Animal bobbY = new Animal("Dog","Bobby", 2015, 4,4, true);
+
+            Console.WriteLine($@"Det var en gång en {bobbY.Age} år gammal {bobbY.Type} som hette {bobbY.Name}. En dag var {bobbY.Name} ute på en promenade I skogen, och mötte en stor varg. Vargen bet av ett ben. {bobbY.Name} sprang {bobbY.IsFast()} hem på sina {bobbY.NumbersOfLegs - 1}.Så var sagan slut.");
+
+            Console.ReadKey();
+        }
+    }
+}
