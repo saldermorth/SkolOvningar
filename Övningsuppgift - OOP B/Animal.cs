3afdcb3580a05bcc3f49eb8a5a13a2bd4643c296
saldermorth <77542775+saldermorth@users.noreply.github.com>
2021-09-24 13:27:13 +0200

    OOP B klar
    OOP  B klar

---
+++ Övningsuppgift - OOP B/Animal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_B
+{// string Name – Namn ska vara Varg, Fladdermus, Delfin, Örn, Häst och några till om du vill.
+
+    class Animal
+    {
+        private string name;
+        public string AnimalBehaviorDay { get; set; }
+        public string AnimalBehaviorNight { get; set; }
+        public bool Nocturnal { get; set; }
+       
+        public string Name {
+            get {return name; }
+            set
+            {
+                Console.WriteLine(name);
+                
+                if (value == "Varg" || value == "Fladdermus" || value == "Delfin" || value == "Örn" || value == "Häst")
+                {
+                    name = value;
+                }
+                else
+                {
+                    name = "No Name";
+                }
+                     } }
+        public Animal(string _name, bool _nocturnal, string _animalBehaviorDay,string _animalBehaviorNight)
+        {
+            Name = _name;
+            Nocturnal = _nocturnal;
+            AnimalBehaviorDay = _animalBehaviorDay;
+            AnimalBehaviorNight = _animalBehaviorNight;
+        }
+        
+    }
+}
---
+++ Övningsuppgift - OOP B/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP_B
+{ /*
+   }
+      OOP : B
+      Skapa ett objekt som har följande egenskaper: ◦ string Name – Namn ska vara Varg, Fladdermus, Delfin, Örn, Häst
+      och några till om du vill. ◦ bool Nocturnal – Nattdjur, sant eller falskt
+      Skapa minst en instans av varje djur, gärna fler, och stoppa i listan
+      forrest. ◦ Välj två knappar, som ska vara dag och natt. ◦ Trycker du på nattknappen ska det stå följande om vargen och
+      fladdermusen (och andra nattdjur): ◦ “Vargen smyger omkring och letar sitt byte” och “Fladdermusen
+      flyger runt bland träden I jakt på mat”. ◦ De djur som inte är nocturnal beskrivs med: Hästen sover, Delfinen sover osv. 
+    ◦ Trycker du på dagknappen så ska det omvända ske, Vargen och
+      Fladdermusen sover, och denadra djuren gör något, t ex letar mat
+      eller springer genom skogen o s v.
+   */
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            bool selection = true;
+            string dayOrNight;
+
+            // string Name – Namn ska vara Varg, Fladdermus, Delfin, Örn, Häst och några till om du vill.
+            List<Animal> forest = new List<Animal>();
+
+            forest.Add(new Animal("Varg", true,"sleeps", "roams tha plaines."));
+            forest.Add(new Animal("Fladdermus", true,"sleeps", "turns to dracula."));
+            forest.Add(new Animal("Delfin", false,"swimms around", "sleeps"));
+            forest.Add(new Animal("Örn", false, "soars", "sleeps in the nest"));
+            forest.Add(new Animal("Häst", false,"jumps things", "sleeps standing up"));
+
+            Console.WriteLine("Write Day or Night");
+            dayOrNight = Console.ReadLine();
+            dayOrNight = dayOrNight.ToLower();
+            if (dayOrNight == "day")
+            {
+                selection = false;
+            }
+            else if(dayOrNight == "night")
+            {
+                selection = true;
+            }
+            else
+            {
+                Console.WriteLine("Incorrect choice");
+                Environment.Exit(1);
+            }
+
+            if (!selection)
+            {
+                foreach (var Animal in forest)
+                {
+                    if (Animal.Nocturnal == true)
+                    {
+                        Console.WriteLine($"{Animal.Name} {Animal.AnimalBehaviorDay}");
+                    } 
+                    if (Animal.Nocturnal == false)
+                    {
+                        Console.WriteLine($"{Animal.Name} {Animal.AnimalBehaviorDay}");
+                    }
+
+                }
+            }
+            else
+            {
+                foreach (var Animal in forest)
+                {
+                    if (Animal.Nocturnal == true)
+                    {
+                        Console.WriteLine($"{Animal.Name} {Animal.AnimalBehaviorNight}");
+                    }
+                    if (Animal.Nocturnal == false)
+                    {
+                        Console.WriteLine($"{Animal.Name} {Animal.AnimalBehaviorNight}");
+                    }
+
+                }
+
+            }
+           
+            
+
+        }
+    }
+}
