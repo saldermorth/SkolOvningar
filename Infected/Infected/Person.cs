0842d4dd1e4925c9153113aa4f6c8edf1f2cd4ed
saldermorth <77542775+saldermorth@users.noreply.github.com>
2021-10-05 14:39:36 +0200

    OOP complete

---
+++ Infected/Infected/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infected
+{
+    public class Person
+    {
+        public bool smittad;
+        public int smittadNär;
+        public bool immun;
+
+        public Person(bool _smittad, int _smittadNär, bool _immun)
+        {
+            smittad = _smittad;
+            smittadNär = _smittadNär;
+            immun = _immun;
+        }
+    }
+}
---
+++ Infected/Infected/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Infected
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int infected = 0;
+            int immun = 0;
+            List<Person> persons = new List<Person>();
+
+
+            for (int i = 0; i < 49; i++)// skapar 49 st personer ej smittade, ej immuna,
+            {
+                Person test = new Person(false, 0 , false);
+                persons.Add(test);
+            }
+
+            Person patientZero = new Person(true, 1, false); // skapar en smittad person.
+            persons.Add(patientZero);
+            for (int j = 0; j < 10; j++) //Timmar
+            {
+                foreach (var item in persons) //infected counter
+                {
+                    if (item.smittad == true && infected < 49)
+                    {
+                        infected++;
+                    }
+                }
+                
+                int newInfected = infected * 2;
+                if (newInfected > 49) // new gets infected
+                {
+                    newInfected = 49;
+                }
+
+                for (int i = 0; i < newInfected; i++)
+                {
+                    if (persons[i].immun == false)
+                    {
+                        persons[i].smittad = true;
+                    }
+
+                }
+
+                foreach (var item in persons)// immun
+                {
+                    if (item.smittadNär > 4)
+                    {
+                        item.immun = true;
+                        item.smittad = false;
+                        if (infected>0)
+                        {
+                            infected--;
+                        }
+                        
+                        if (immun< 50)
+                        {
+                            immun++;
+                        }
+                       
+                    }
+                }
+                foreach (var item in persons)
+                {
+                    if (item.smittad == true)
+                    {
+                        item.smittadNär++;
+                    }
+                }
+                
+                Console.WriteLine($"Immuna : {immun}  | Infected : {infected} ");
+            }
+            
+
+            Console.ReadLine();
+        }
+    }
+}
