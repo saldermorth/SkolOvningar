dbd63290850b89a3696482a4b26b5c732cda507e
saldermorth <77542775+saldermorth@users.noreply.github.com>
2021-10-05 14:00:45 +0200

    Merry Method add

---
+++ MerryMethodsSolution/MerryMethodsProject/MethodBench.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MerryMethodsProject
+{
+    public class MethodBench
+    {
+        public void FirstMethod()
+        {
+            Console.WriteLine("Merry Method");
+        }
+        public void SecondMethod(string s)
+        {
+           
+            Console.WriteLine(s);
+        }
+        public void ThirdMethod(string s , bool b)
+        {
+                        
+            if (b)
+            {
+                s = s.ToUpper();
+            }
+            else
+            {
+                s = s.ToLower();
+            }
+            SecondMethod(s);
+        }
+        public bool FourthMethod()
+        {
+            bool shout = false;
+            Console.WriteLine("Ska datorn (S)krika eller (V)iska?");
+
+
+            if (Console.ReadKey().Key != ConsoleKey.S)
+            {
+                shout = false;
+            }
+            else if (Console.ReadKey().Key != ConsoleKey.V)
+            {
+                shout = true;
+            }
+            return shout;
+
+        }
+    }
+}
---
+++ MerryMethodsSolution/MerryMethodsProject/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MerryMethodsProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool shout = false;
+            MethodBench bench = new MethodBench();
+            int i = 3;
+            do
+            {
+                bench.FirstMethod();
+                i--;
+            } while (i>0);
+            
+            string s = "C# Now and Forever!";
+            bench.SecondMethod("C# Now and Forever!");
+            bench.SecondMethod(s);
+            Console.WriteLine("String to (1)upper case or (2)lower case? ");
+            int upperOrLower = Convert.ToInt32(Console.ReadLine());
+            bool upper = false;
+            if (upperOrLower == 1)
+            {
+                upper = true;
+            }
+            else if (upperOrLower == 2)
+            {
+                upper = false;
+            }
+            bench.ThirdMethod(s, upper);
+            shout = bench.FourthMethod();
+            bench.ThirdMethod(s, shout);
+        }
+    }
+}
